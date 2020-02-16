72ddec89bce3372e4b53bb7cb891b1950fbee56a
Zaidrakhman Miras <miras.zaidrakhman@outlook.com>
2020-02-16 22:59:17 +0600

    added ef repository

--- EFDedWeb/Pages/Index.cshtml.cs
+++ EFDedWeb/Pages/Index.cshtml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +14,26 @@ namespace EFDedWeb.Pages
     public class IndexModel:PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly PeopleContext _db;
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
 
+        }
+        public void LoadSampleData() 
+        {
+            if (_db.People.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                _db.AddRange(people);
+                _db.SaveChanges();
+            }
         }
     }
 }
